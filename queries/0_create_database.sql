aed5e7b9257eb28b8b6d415d01501e4bc7667f37
witzkvn <witz.kevin05@gmail.com>
2023-05-23 15:56:56 +0200

    ajout creation db et insert jeu de donnees

--- queries/0_create_database.sql
+++ queries/0_create_database.sql
@@ -1,3 +1,57 @@
 # Suit les instruction du checkpoint, en utilisant https://www.dbdesigner.net/, créer un script
 # d'initialisation de BDD SQLite et copie colle son contenu ici
 # (tu dois notmalement avoir +/- 4 instructions CREATE TABLE, chaque instructions doit bien se terminer par un ";")
+
+CREATE TABLE candidate(
+   Id_candidate INTEGER,
+   login TEXT NOT NULL,
+   password TEXT NOT NULL,
+   lastname TEXT NOT NULL,
+   firstname TEXT NOT NULL,
+   email TEXT NOT NULL,
+   phone TEXT,
+   description TEXT,
+   PRIMARY KEY(Id_candidate),
+   UNIQUE(login),
+   UNIQUE(email)
+);
+
+CREATE TABLE recruiter(
+   Id_recruiter INTEGER,
+   login TEXT NOT NULL,
+   password TEXT NOT NULL,
+   PRIMARY KEY(Id_recruiter),
+   UNIQUE(login)
+);
+
+CREATE TABLE business(
+   Id_business INTEGER,
+   name TEXT NOT NULL,
+   description TEXT,
+   Id_recruiter INTEGER NOT NULL,
+   PRIMARY KEY(Id_business),
+   UNIQUE(Id_recruiter),
+   FOREIGN KEY(Id_recruiter) REFERENCES recruiter(Id_recruiter)
+);
+
+CREATE TABLE offer(
+   Id_offer INTEGER,
+   title TEXT NOT NULL,
+   description TEXT NOT NULL,
+   city TEXT NOT NULL,
+   Id_business INTEGER NOT NULL,
+   PRIMARY KEY(Id_offer),
+   FOREIGN KEY(Id_business) REFERENCES business(Id_business)
+);
+
+CREATE TABLE candidate_offer(
+   Id_candidate INTEGER,
+   Id_offer INTEGER,
+   PRIMARY KEY(Id_candidate, Id_offer),
+   FOREIGN KEY(Id_candidate) REFERENCES candidate(Id_candidate),
+   FOREIGN KEY(Id_offer) REFERENCES offer(Id_offer)
+);
+
+
+
+
--- queries/1_insert_data.sql
+++ queries/1_insert_data.sql
@@ -1,3 +1,29 @@
 # Écris dans ce fichier quelques instructions SQL pour insérer de données dans ta BDD
 # N'oublie pas, les requêtes SQL doivent se terminer avec un ";"
 # Pense à céer au moins une entreprise nommée "WCS", à créer des offre d'emploi à "Paris" dont une qui s'intitule "Dev"
+
+INSERT INTO recruiter ('login', 'password') VALUES ('userOne', 'ABcd1234*');
+
+INSERT INTO business ('name', 'Id_recruiter', 'description')
+VALUES ('WCS', (SELECT Id_recruiter FROM recruiter WHERE login = 'userOne'), 'Ecole du numérique');
+
+
+INSERT INTO offer ('title', 'description', 'city', 'Id_business') 
+VALUES ('Dev', 'Super poste de dev JS', 'Paris', (SELECT Id_business FROM business WHERE name = 'WCS'));
+
+INSERT INTO offer ('title', 'description', 'city', 'Id_business') VALUES ('SEM', 'Student Experience Manager', 'Strasbourg', (SELECT Id_business FROM business WHERE name = 'WCS'));
+
+INSERT INTO candidate ('login', 'password', 'lastname', 'firstname', 'email', 'phone', 'description')
+VALUES ('candidate1', 'ABcd1234*', 'Doe', 'Jane', 'jane.doe@example.com', '123456789', 'Description du candidat 1');
+
+INSERT INTO candidate_offer (Id_candidate, Id_offer) VALUES ((SELECT Id_candidate FROM candidate WHERE login = 'candidate1'), (SELECT Id_offer FROM offer WHERE title = 'Dev'));
+
+INSERT INTO candidate ('login', 'password', 'lastname', 'firstname', 'email', 'phone', 'description') VALUES ('candidate2', 'ABcd1234*', 'Smith', 'John', 'john.smith@example.com', '987654321', 'Description du candidat 2');
+
+INSERT INTO candidate_offer (Id_candidate, Id_offer) VALUES ((SELECT Id_candidate FROM candidate WHERE login = 'candidate2'), (SELECT Id_offer FROM offer WHERE title = 'Dev'));
+
+INSERT INTO candidate ('login', 'password', 'lastname', 'firstname', 'email', 'phone', 'description') VALUES ('candidate3', 'ABcd1234*', 'Johnson', 'Emily', 'emily.johnson@example.com', '555555555', 'Description du candidat 3');
+
+INSERT INTO candidate_offer (Id_candidate, Id_offer) VALUES ((SELECT Id_candidate FROM candidate WHERE login = 'candidate3'), (SELECT Id_offer FROM offer WHERE title = 'SEM'));
+
+INSERT INTO candidate ('login', 'password', 'lastname', 'firstname', 'email', 'phone', 'description') VALUES ('candidate4', 'ABcd1234*', 'Bros', 'Mario', 'mario.bros@example.com', '988854322', 'Description du candidat 4');
